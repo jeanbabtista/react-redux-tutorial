f3431290849a08570f2e28ef0a525a9b056d9ca3
Jean <bedrac.zan@gmail.com>
2022-06-29 09:11:30 +0200

    Sample counter Redux store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { actions } from './store'
 
 function App() {
+  const counter = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(actions.increment())}>++</button>
+      <button onClick={() => dispatch(actions.decrement())}>--</button>
+      <button onClick={() => dispatch(actions.add(5))}>+5</button>
+    </>
+  )
 }
 
-export default App;
+export default App
